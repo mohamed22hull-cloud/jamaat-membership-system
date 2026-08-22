cdcad1b18255fe853409d9eedf3daaa90c14c9f2
mohamed22hull-cloud <mohamed22hull@gmail.com>
2026-08-22 11:38:44 -0700

    Add files via upload

---
+++ locations.js
@@ -0,0 +1,240 @@
+const locations = {
+
+    "Bo": {
+        "Badja": [],
+        "Bagbe": [],
+        "Bagbo": [],
+        "Baoma": [],
+        "Bumpe": [],
+        "Gbo": [],
+        "Jaiama": [],
+        "Bongor": [],
+        "Kakua": [],
+        "Komboya": [],
+        "Lugbu": [],
+        "Niawa-Lenga": [],
+        "Selenga": [],
+        "Tikonko": [],
+        "Valunia": [],
+        "Wonde": []
+    },
+
+    "Bombali": {
+        "Bombali Sebora": [],
+        "Bombali Siaray": [],
+        "Makari": [],
+        "Gbanti": [],
+        "Paki Masabong": [],
+        "Safroko Limba": [],
+        "Biriwa Limba": [],
+        "Gbendembu": [],
+        "Ngowahun": [],
+        "Magbaiamba-Ndowahun": [],
+        "Kamaranka": [],
+        "Mara": []
+    },
+
+    "Falaba": {
+        "Neya": [],
+        "Nyedu": [],
+        "Kulor-Seradu": [],
+        "Mongo": [],
+        "Seradu": [],
+        "Morifindugu": [],
+        "Delemandugu": [],
+        "Sulima": [],
+        "Kabelia": [],
+        "Folasaba-Kamba": [],
+        "Dembelia Musaia": [],
+        "Dembelia Sikunia": [],
+        "Kamadugu-Yiraia": []
+    },
+
+    "Koinadugu": {
+        "Wara-Wara Bafodia": [],
+        "Kamukeh": [],
+        "Sengbe": [],
+        "Wara-Wara Yagalla": [],
+        "Diang": [],
+        "Kasunko-Kakellay": [],
+        "Gbongobor-Kayaka": [],
+        "Thamiso": [],
+        "Neini": [],
+        "Kallian": [],
+        "Barawa-Wolley": []
+    },
+
+    "Tonkolili": {
+        "Dansogoia": [],
+        "Kalantuba": [],
+        "Kafe": [],
+        "Simiria": [],
+        "Kholifa Rowala": [],
+        "Tane": [],
+        "Kunike Gbarnga": [],
+        "Kholifa Mayosso-Mamuntha": [],
+        "Kunike Sanda": [],
+        "Kunike Folawusu": [],
+        "Yele": [],
+        "Mayeppoh": [],
+        "Poli": [],
+        "Masakong": [],
+        "Yoni Mabanta": [],
+        "Yoni Mamailla": [],
+        "Kholifa Mabang": [],
+        "Malal": [],
+        "Sambaia": []
+    },
+
+    "Kambia": {
+        "Magbema": [],
+        "Mambolo": [],
+        "Samu": [],
+        "Gbinle": [],
+        "Dixon": [],
+        "Tonko Limba": [],
+        "Bramaia": [],
+        "Khonimakha": [],
+        "Masungbala": [],
+        "Tala-Munu": []
+    },
+
+    "Port Loko": {
+        "Bureh": [],
+        "Kasseh": [],
+        "Maconteh": [],
+        "Bake-Loko": [],
+        "Maforki": [],
+        "Lokomassama": [],
+        "Kamasondo": [],
+        "Kaffu Bullom": [],
+        "Marampa": [],
+        "Koya": [],
+        "Masimera": [],
+        "Thainkatopa": [],
+        "Makama": []
+    },
+
+    "Karene": {
+        "Tambakha Simibungie": [],
+        "Tambakha Yobangie": [],
+        "Gbanti": [],
+        "Libeisaygahun-Gombagu": [],
+        "Mafonda": [],
+        "Makerembay": [],
+        "Sanda Tendaren": [],
+        "Sanda Magbolontor": [],
+        "Sella Limba": [],
+        "Sanda Loko": [],
+        "Buya": [],
+        "Romende": [],
+        "Safroko": [],
+        "Dibia": []
+    },
+
+    "Bonthe": {
+        "Bendu Cha": [],
+        "Bum": [],
+        "Dema": [],
+        "Imperi": [],
+        "Jong": [],
+        "Kpanda-Kemo": [],
+        "Kwamebai Krim": [],
+        "Nongoba Bullom": [],
+        "Sittia": [],
+        "Sogbini": [],
+        "Yawbeko": []
+    },
+
+    "Moyamba": {
+        "Bagruwa": [],
+        "Banta": [],
+        "Banta-Mokele": [],
+        "Bumpe": [],
+        "Dasse": [],
+        "Fakunya": [],
+        "Kaiyamba": [],
+        "Kagboro": [],
+        "Kamajei": [],
+        "Kongboro": [],
+        "Kori": [],
+        "Kowa": [],
+        "Ribbi": [],
+        "Timdel": []
+    },
+
+    "Pujehun": {
+        "Barri": [],
+        "Gallines": [],
+        "Perri": [],
+        "Kpaka": [],
+        "Makpele": [],
+        "Malen": [],
+        "Mano Sakrim": [],
+        "Kpanga": [],
+        "Kabonde": [],
+        "Kpanga-Krim": [],
+        "Peje": [],
+        "Soro Gbema": [],
+        "Sowa": [],
+        "Yekemo-Kpukumu-Krim": []
+    },
+
+    "Kailahun": {
+        "Dea": [],
+        "Njaluahun": [],
+        "Kissi Kama": [],
+        "Kissi Teng": [],
+        "Kissi Tongi": [],
+        "Jawei": [],
+        "Luawa": [],
+        "Malema": [],
+        "Mandu": [],
+        "Peje West": [],
+        "Penguia": [],
+        "Upper Bambara": [],
+        "Kpeje Bongre": [],
+        "Yawei": []
+    },
+
+    "Kenema": {
+        "Dama": [],
+        "Dodo": [],
+        "Gaura": [],
+        "Gorama Mende": [],
+        "Kandu Leppiama": [],
+        "Koya": [],
+        "Langrama": [],
+        "Lower Bambara": [],
+        "Malegohun": [],
+        "Niawa": [],
+        "Nomo": [],
+        "Nongowa": [],
+        "Simbaru": [],
+        "Small Bo": [],
+        "Tunkia": [],
+        "Wando": []
+    },
+
+    "Kono": {
+        "Tankoro": [],
+        "Gbense": [],
+        "Kama'a": [],
+        "Nimikoro": [],
+        "Nimiyama": [],
+        "Sandor": [],
+        "Toli": [],
+        "Gbane": [],
+        "Lei": [],
+        "Soa": [],
+        "Fiama": [],
+        "Gorama Kono": [],
+        "Mafindor": [],
+        "Gbane Kandor": []
+    },
+
+    "Western Area Rural": {},
+
+    "Western Area Urban": {}
+
+};
